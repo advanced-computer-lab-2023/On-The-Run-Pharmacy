10833835c2af4d43b308bf933c5976670b940900
Yousef-Networks <121651007+Yousef-Networks@users.noreply.github.com>
2023-11-06 20:08:08 +0200

    Add files via upload

---
+++ PharmaRequest.js
@@ -0,0 +1,206 @@
+import React,{ useState } from 'react';
+import { Link,useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const PharmaRegistration = () => {
+    const navigate = useNavigate();
+
+    const[username,setUsername]=useState('')
+const[name,setName]=useState('')
+const[email,setEmail]=useState('')
+const[password,setPassword]=useState('')
+const[hourly_rate,setHourly_rate]=useState('')
+const[affiliation,setAffiliation]=useState('')
+const [educational_background, setEducational_background] = useState('');
+
+const [workingLicense, setWorkingLicense] = useState(null);
+const [pharmacistDegree, setPharmacistDegree] = useState(null);
+const [pharmacistId, setPharmacistId] = useState(null);
+
+const [error, setError] = useState(null);
+const [isPharmacistRegistered, setIsPharmacistRegistered] = useState(false);
+
+  
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const p={username,name,email,password,hourly_rate,affiliation,educational_background,workingLicense,pharmacistDegree,pharmacistId}
+        try {
+          // Make a POST request to your backend API endpoint
+          const response = await axios.post('http://localhost:4000/createRequest', p);
+      
+          if (response.status === 201) {
+            console.log('Registration successful:', response.data);
+            setUsername('');
+            setName('');
+            setEmail('');
+            setPassword('');
+            setHourly_rate('');
+            setAffiliation('');
+            setEducational_background('');
+            setWorkingLicense(null);
+            setPharmacistDegree(null);
+            setPharmacistId(null);
+            setError(null);
+            setIsPharmacistRegistered(true);
+           // navigate(`/dashboard/patient/${username}`);
+            console.log('Registration successful', response.data);
+          }
+        } catch (error) {
+          // Handle errors, e.g., display an error message to the user
+          console.error('Error registering pharmacist:', error);
+        }
+      };
+
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <div className="col-lg-6">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="card-title">Pharmacist Registration</h1>
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="username">Username</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="username"
+                    onChange={(e)=>setUsername(e.target.value)}
+            value={username}
+                    placeholder="Enter your username"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    value={name}
+            onChange={(e)=>setName(e.target.value)}
+                    placeholder="Enter your name"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+                    placeholder="Enter your email"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+                    placeholder="Enter your password"
+                  />
+                </div>
+
+               
+
+                
+
+                <div className="form-group">
+                  <label htmlFor="hourlyRate">Hourly Rate</label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="hourlyRate"
+                    value={hourly_rate}
+            onChange={(e) => setHourly_rate(e.target.value)}
+                    placeholder="Enter your hourly rate"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="aff">Affiliation</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="emergencyContactName"
+                    value={affiliation}
+            onChange={(e) =>
+             setAffiliation(e.target.value)
+            }
+                    placeholder="Enter you Affiliation"
+                  />
+                </div>
+
+                
+
+                <div className="form-group">
+                  <label htmlFor="educational_background">Educational Background</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="educational_background"
+                    value={educational_background}
+            onChange={(e) =>
+              setEducational_background(e.target.value)
+            }
+                    placeholder="Enter your Educational Background"
+                  />
+                </div>
+                  {/* Working License PDF */}
+                  <div className="form-group">
+                  <label htmlFor="workingLicense">Working License PDF</label>
+                  <input
+                    type="file"
+                    id="workingLicense"
+                    accept=".pdf"
+                    onChange={(e) => setWorkingLicense(e.target.files[0])}
+                  />
+                </div>   
+                {/* Pharmacist Degree PDF */}
+                <div className="form-group">
+                  <label htmlFor="pharmacistDegree">Pharmacist Degree PDF</label>
+                  <input
+                    type="file"
+                    id="pharmacistDegree"
+                    accept=".pdf"
+                    onChange={(e) => setPharmacistDegree(e.target.files[0])}
+                  />
+                </div>
+                {/* Pharmacist ID PDF */}
+                <div className="form-group">
+                  <label htmlFor="pharmacistId">Pharmacist ID PDF</label>
+                  <input
+                    type="file"
+                    id="pharmacistId"
+                    accept=".pdf"
+                    onChange={(e) => setPharmacistId(e.target.files[0])}
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Register
+                </button>
+              </form>
+
+              <div className="mt-3">
+                <Link to="/" className="btn btn-secondary">
+                  Back to Role Selection
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PharmaRegistration;
